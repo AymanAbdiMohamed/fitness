07a1ef8e3f3e12d95ceb573ae321d9e8a00ee612
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2026-02-28 15:54:29 +0300

    feat: created app bar that containes title, left arrow action and settings action

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,12 +6,59 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
       ),
-      body: const Center(
-        child: Text('Welcome to the Home Page!'),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: const Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Image.asset(
+            'assets/icons/arrowleft.png',
+            height: 20,
+            width: 20,
+          ),
+        ),
       ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            width: 37,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: const Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image.asset(
+              'assets/icons/dots.png',
+              height: 5,
+              width: 5,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
